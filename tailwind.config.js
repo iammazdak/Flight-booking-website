8659cb1f94ddb5aa5455d8f259b07e511c482780
Mazdak <Mazdak.es23@gmail.com>
2023-06-09 01:13:23 +0330

    project starter

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'Black': '#151822',
+      'White': '#FFFFFF',
+      'Yellow': '#FFB039',
+      'Dark-blue': '#00203C',
+      'Light-gray': '#F1F2F9',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
