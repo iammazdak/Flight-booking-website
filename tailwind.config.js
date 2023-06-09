e02f96bea562087ddc4f7d4ebb9059f510872a9b
saeed <ghansaryan.saeed@gmail.com>
2023-06-09 12:07:32 +0330

    change config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,28 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // },
+    // colors: {
+    //   'Black': '#151822',
+    //   'White': '#FFFFFF',
+    //   'Yellow': '#FFB039',
+    //   'Dark-blue': '#00203C',
+    //   'Light-gray': '#F1F2F9',
+    // },
+    extend: {
+      colors: {
+        'Black': '#151822',
+        'White': '#FFFFFF',
+        'Yellow': '#FFB039',
+        'Dark-blue': '#00203C',
+        'Light-gray': '#F1F2F9',
+      },
     },
-    colors: {
-      'Black': '#151822',
-      'White': '#FFFFFF',
-      'Yellow': '#FFB039',
-      'Dark-blue': '#00203C',
-      'Light-gray': '#F1F2F9',
-    },
-    extend: {},
   },
   plugins: [],
 }
