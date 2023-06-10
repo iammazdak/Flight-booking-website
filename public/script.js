14292c71ddfee68d8cc5efb83bb5b322f5a09efe
Mazdak <Mazdak.es23@gmail.com>
2023-06-10 10:48:27 +0330

    edited mega menu

--- public/script.js
+++ public/script.js
@@ -2,11 +2,14 @@ let $ = document
 
 let megamenuBtn = $.getElementById("megamenu-btn")
 let megaMenu = $.getElementById("megamenu")
+let megaMenubtnIcon = $.getElementById("megamenu-btn-icon")
 
 megamenuBtn.addEventListener("click", function () {
     if (megaMenu.style.display === "none") {
         megaMenu.style.display = "flex"
+        megaMenubtnIcon.style.rotate = "180deg"
     } else {
         megaMenu.style.display = "none"
+        megaMenubtnIcon.style.rotate = "360deg"
     }
 })
