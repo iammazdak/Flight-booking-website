24c59a6dc74ca8f3f371d499b511e47339beb4a0
Mazdak <Mazdak.es23@gmail.com>
2023-06-10 18:10:20 +0330

    optimized js

--- public/script.js
+++ public/script.js
@@ -5,14 +5,19 @@ let megaMenu = $.getElementById("megamenu")
 let megaMenubtnIcon = $.getElementById("megamenu-btn-icon")
 let body = $.getElementById("body")
 
+// megamenuBtn.addEventListener("click", function () {
+//     if (megaMenu.style.display === "none") {
+//         body.style.overflow = "hidden"
+//         megaMenu.style.display = "flex"
+//         megaMenubtnIcon.style.rotate = "180deg"
+//     } else {
+//         body.style.overflow = "auto"
+//         megaMenu.style.display = "none"
+//         megaMenubtnIcon.style.rotate = "360deg"
+//     }
+// })
 megamenuBtn.addEventListener("click", function () {
-    if (megaMenu.style.display === "none") {
-        body.style.overflow = "hidden"
-        megaMenu.style.display = "flex"
-        megaMenubtnIcon.style.rotate = "180deg"
-    } else {
-        body.style.overflow = "auto"
-        megaMenu.style.display = "none"
-        megaMenubtnIcon.style.rotate = "360deg"
-    }
+    megaMenu.classList.toggle('hidden');
+
+    megaMenubtnIcon.classList.toggle('rotate-180')
 })
