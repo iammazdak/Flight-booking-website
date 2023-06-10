a81862548f983c884b0526cad4e7fa9c287595e8
saeed <ghansaryan.saeed@gmail.com>
2023-06-10 14:00:42 +0330

    update script file

--- public/script.js
+++ public/script.js
@@ -3,12 +3,15 @@ let $ = document
 let megamenuBtn = $.getElementById("megamenu-btn")
 let megaMenu = $.getElementById("megamenu")
 let megaMenubtnIcon = $.getElementById("megamenu-btn-icon")
+let body = $.getElementById("body")
 
 megamenuBtn.addEventListener("click", function () {
     if (megaMenu.style.display === "none") {
+        body.style.overflow = "hidden"
         megaMenu.style.display = "flex"
         megaMenubtnIcon.style.rotate = "180deg"
     } else {
+        body.style.overflow = "auto"
         megaMenu.style.display = "none"
         megaMenubtnIcon.style.rotate = "360deg"
     }
