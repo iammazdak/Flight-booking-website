024e5a1266784f4c6902072541ea63ad15d0bdb1
saeed <ghansaryan.saeed@gmail.com>
2023-06-27 16:58:35 +0330

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: ["./public/**/*.html", "./public/*.js"],
   theme: {
     extend: {
       colors: {
