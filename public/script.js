3e2733375e11705e7fa565829ee6a8ba0019fdc0
saeed <ghansaryan.saeed@gmail.com>
2023-06-27 16:58:58 +0330

    update scripts

--- public/script.js
+++ public/script.js
@@ -1,53 +1,57 @@
-let $ = document;
+const megamenu              = document.getElementById("megamenu");
+const megamenuButton        = document.getElementsByClassName("megamenu-button")[0];
+const megamenuIcon          = document.getElementsByClassName("megamenu-icon")[0];
 
-let megamenuBtn = $.querySelector('#megamenu-btn');
-let megaMenu = $.querySelector('#megamenu');
-let megaMenubtnIcon = $.querySelector('#megamenu-btn-icon');
-let sidebarMenu = $.querySelector('#sidebar-menu');
-let mobileMenu = $.querySelector('#mobile-menu');
+const mobileMenu            = document.getElementById("mobile-menu");
+const mobileMenuButton      = document.getElementsByClassName("mobile-menu-button")[0];
+const mobileMenuButtonClose = document.getElementsByClassName("mobile-menu-button-close")[0];
 
-megamenuBtn.addEventListener('mouseover', ()=> {
+const expandDrawer          = document.getElementById("expand-drawer");
+const expandDrawerButton    = document.getElementsByClassName("expand-drawer-button")[0];
+const expandDrawerIcon      = document.getElementsByClassName("expand-drawer-icon")[0];
 
-    megaMenuOpen();
-    megaMenubtnIcon.classList.toggle('rotate-180')
-});
+const body                  = document.body;
 
-megamenuBtn.addEventListener('mouseout', ()=> {
-
-    megaMenuClose();
-    megaMenubtnIcon.classList.toggle('rotate-180')
-});
-
-sidebarMenu.addEventListener('click', ()=> {
-
-    mobileMenuToggle();
-})
-
-function megaMenuOpen(_true) {
-
-    if ( megaMenu.classList.contains('hidden') ) {
-
-        $.body.classList.remove('overflow-auto');
-        $.body.classList.add('overflow-hidden');
-        megaMenu.classList.remove('hidden');
-        megaMenu.classList.add('flex');
-    } 
-    return _true;
-}
-
-function megaMenuClose(_true) {
-
-    if ( megaMenu.classList.contains('flex') ) {
-
-        $.body.classList.remove('overflow-hidden');
-        $.body.classList.add('overflow-auto');
-        megaMenu.classList.add('hidden');
-        megaMenu.classList.remove('flex');
+// Toggle megamenu visibility
+function toggleMegamenu(event) {
+    if (event.type === "mouseenter" || !megamenu.contains(event.relatedTarget)) {
+        megamenu.classList.toggle("hidden");
+        megamenu.classList.toggle("flex");
+        megamenuIcon.classList.toggle("rotate-180");
+        body.classList.toggle("overflow-hidden");
     }
-    return _true;
 }
 
-function mobileMenuToggle() {
+// Add mouse event listeners to megamenuButton and megamenu
+megamenuButton.addEventListener("mouseenter", toggleMegamenu);
+megamenuButton.addEventListener("mouseleave", toggleMegamenu);
+megamenu.addEventListener("mouseleave", toggleMegamenu);
 
-    mobileMenu.classList.toggle('hidden');
+// Toggle mobile menu visibility
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+
+    // Toggle mobileMenuButton and mobileMenuButtonClose classes
+    if (mobileMenu.classList.contains("hidden")) {
+        mobileMenuButton.classList.remove("hidden");
+        mobileMenuButtonClose.classList.add("hidden");
+    } else {
+        mobileMenuButton.classList.add("hidden");
+        mobileMenuButtonClose.classList.remove("hidden");
+    }
 }
+
+if (mobileMenuButton && mobileMenu) {
+    mobileMenuButton.onclick = toggleMobileMenu;
+}
+
+if (mobileMenuButtonClose && mobileMenu) {
+    mobileMenuButtonClose.onclick = toggleMobileMenu;
+}
+
+// Toggle expand drawer visibility
+expandDrawerButton.addEventListener("click", function() {
+    expandDrawer.classList.toggle("hidden");
+    expandDrawerIcon.classList.toggle("rotate-180");
+});
